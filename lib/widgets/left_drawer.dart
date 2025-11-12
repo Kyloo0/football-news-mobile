1dac0fa383d1932641234b53e0c6159e078e8e60
Kyloo0 <voltaniefitto@gmail.com>
2025-11-12 16:09:54 +0700

    tutorial 8 done

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
 import 'package:football_news/screens/newlist_form.dart';
+import 'package:football_news/screens/news_entry_list.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -79,6 +80,17 @@ class LeftDrawer extends StatelessWidget {
                   builder: (context) => MyHomePage(), // Ganti apabila sudah implement Page view news nya
                 ));
             }
+          ),
+          ListTile(
+              leading: const Icon(Icons.add_reaction_rounded),
+              title: const Text('News List'),
+              onTap: () {
+                  // Route to news list page
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const NewsEntryListPage()),
+                  );
+              },
           ),
         ],
       ),
